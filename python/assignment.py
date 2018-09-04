fed2b64187227a8c49699199ad7f42543b1cf4d9
williamvage <williamvage@hotmail.com>
2018-09-04 19:19:24 +0200

    added calculation of entropy

---
+++ python/assignment.py
@@ -0,0 +1,13 @@
+# authors: oolosson, wvage
+
+import monkdata as m
+from dtree import *
+
+
+def calculate_entropy():
+    m.monk1
+    print("entropy of monk1:", entropy(m.monk1))
+    print("entropy of monk2:", entropy(m.monk2))
+    print("entropy of monk3:", entropy(m.monk3))
+
+calculate_entropy()
