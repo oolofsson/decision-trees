54dde8617c402352a59590ada916c87ad08fdd62
williamvage <williamvage@hotmail.com>
2018-09-04 22:21:13 +0200

    added calculation for information gain of attributes in the different MONK datasets

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # decision-trees
-Machine Learning Laboration: Decision Trees
+Machine Learning Laboration 1: Decision Trees
+
+## Assignment 0
+
+## Assignment 1
+
+## Assignment 2
+
+## Assignment 3
+
+## Assignment 4
+
+## Assignment 5
+
+## Assignment 6
+
+## Assignment 7
--- python/assignment.py
+++ python/assignment.py
@@ -9,4 +9,36 @@ def calculate_entropy():
     print("entropy of monk2:", entropy(m.monk2))
     print("entropy of monk3:", entropy(m.monk3))
 
-calculate_entropy()
+def calculate_average_gain():
+    #monk1
+    print("average gain of a1 in monk1", averageGain(m.monk1, m.attributes[0]))
+    print("average gain of a2 in monk1", averageGain(m.monk1, m.attributes[1]))
+    print("average gain of a3 in monk1", averageGain(m.monk1, m.attributes[2]))
+    print("average gain of a4 in monk1", averageGain(m.monk1, m.attributes[3]))
+    print("average gain of a5 in monk1", averageGain(m.monk1, m.attributes[4]))
+    print("average gain of a6 in monk1", averageGain(m.monk1, m.attributes[5]))
+    print("")
+
+    #monk2
+    print("average gain of a1 in monk2", averageGain(m.monk2, m.attributes[0]))
+    print("average gain of a2 in monk2", averageGain(m.monk2, m.attributes[1]))
+    print("average gain of a3 in monk2", averageGain(m.monk2, m.attributes[2]))
+    print("average gain of a4 in monk2", averageGain(m.monk2, m.attributes[3]))
+    print("average gain of a5 in monk2", averageGain(m.monk2, m.attributes[4]))
+    print("average gain of a6 in monk2", averageGain(m.monk2, m.attributes[5]))
+    print("")
+    
+    #monk3
+    print("average gain of a1 in monk3", averageGain(m.monk3, m.attributes[0]))
+    print("average gain of a2 in monk3", averageGain(m.monk3, m.attributes[1]))
+    print("average gain of a3 in monk3", averageGain(m.monk3, m.attributes[2]))
+    print("average gain of a4 in monk3", averageGain(m.monk3, m.attributes[3]))
+    print("average gain of a5 in monk3", averageGain(m.monk3, m.attributes[4]))
+    print("average gain of a6 in monk3", averageGain(m.monk3, m.attributes[5]))
+
+
+def main():
+    #calculate_entropy()
+    calculate_average_gain()
+
+main()
